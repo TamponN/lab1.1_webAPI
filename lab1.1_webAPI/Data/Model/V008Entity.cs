ef718d04cacecd921f4460c12367d35aeb899c4b
bob <147228055+TamponN@users.noreply.github.com>
2024-10-17 22:51:14 +0400

    Edited Some shit

--- lab1.1_webAPI/Data/Model/V008Entity.cs
+++ lab1.1_webAPI/Data/Model/V008Entity.cs
@@ -1,9 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Reflection.Metadata.Ecma335;
-using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
 
 namespace Data.Model
